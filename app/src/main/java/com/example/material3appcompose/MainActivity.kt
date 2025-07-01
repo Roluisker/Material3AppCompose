5cfad8dae4a17b5166cff047f59b0804e659a755
Luis Bejarano <roluisker@gmail.com>
2025-07-01 13:29:52 -0500

    first commit

---
+++ app/src/main/java/com/example/material3appcompose/MainActivity.kt
@@ -0,0 +1,263 @@
+package com.example.material3appcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.example.material3appcompose.ui.theme.Material3AppComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Material3AppComposeTheme {
+
+                var showDialog by remember {
+                    mutableStateOf(false)
+                }
+
+                if (showDialog) {
+                    Material3AlertDialog(onDismiss = {
+                        showDialog = false
+                    })
+                }
+
+                Scaffold(
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {
+                            showDialog = true
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Call,
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    topBar = {
+                        Material3TopAppBar()
+                    },
+                    bottomBar = {
+                        Material3BottomBar()
+                    }) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        Column(
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .padding(vertical = 10.dp)
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            MaterialDogCard()
+                            MaterialDogCard()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MaterialDogCard() {
+        var expanded by remember {
+            mutableStateOf(false)
+        }
+
+        Card(
+            onClick = {
+                expanded = !expanded
+            },
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+            ),
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 15.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateContentSize()
+            ) {
+                Column {
+                    AsyncImage(
+                        model = "https://images.unsplash.com/photo-1561037404-61cd46aa615b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.4f),
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(
+                        "This is my Dog",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(horizontal = 5.dp, vertical = 10.dp)
+                    )
+                    Text(
+                        "Mi perrito peludo y juugueton bla bla bla whof whjof whof whof" +
+                                "Mi perrito peludo y juugueton bla bla bla whof whjof whof whof" +
+                                "Mi perrito peludo y juugueton bla bla bla whof whjof whof whof" +
+                                "Mi perrito peludo y juugueton bla bla bla whof whjof whof whof",
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(horizontal = 5.dp, vertical = 5.dp),
+                        maxLines = if (expanded) 20 else 3,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun Material3BottomBar() {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            actions = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = null
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.Create,
+                        contentDescription = null
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = null
+                    )
+                }
+            }
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun Material3TopAppBar() {
+        TopAppBar(
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.Create,
+                        contentDescription = null
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = null
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+            ),
+            title = {
+                Text(
+                    color = Color.White,
+                    text = "material 3 app"
+                )
+            })
+    }
+
+    @Composable
+    fun Material3AlertDialog(onDismiss: () -> Unit) {
+        AlertDialog(
+            onDismissRequest = {
+                onDismiss()
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    onDismiss()
+                }) {
+                    Text(text = "Yes, call my Dog")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    onDismiss()
+                }) {
+                    Text(text = "Nop, no yet")
+                }
+            },
+            title = {
+                Text(text = "Title call my dog")
+            },
+            text = {
+                Text(text = "Are you sure wan call my dog")
+            })
+    }
+}
